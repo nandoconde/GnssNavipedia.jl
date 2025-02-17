9af9977ef313cd0ce1943927b1dfe66591ed365e
Fernando <fernando.conde.pumpido@outlook.com>
2025-02-18 00:12:00 +0100

    Intermediate status

--- src/GnssNavipedia.jl
+++ src/GnssNavipedia.jl
@@ -1,5 +1,52 @@
 module GnssNavipedia
 
-# Write your package code here.
+using Moshi.Data.Prelude
+
+import GnssCore: AbstractNavipedia, AbstractConstellation, AbstractSignalBand, AbstractSignalComponent
+import GnssCore: get_constellations, get_signal_bands, get_signal_types
+
+struct Navipedia <: AbstractNavipedia end
+
+@data Constellations <: AbstractConstellation begin
+    GPS
+    GALILEO
+    GLONASS
+end
+
+@data SignalBands <: AbstractSignalBand begin
+    L1
+    L2
+    L5
+    E1
+    E5
+    E6
+end
+
+@data SignalComponents <: AbstractSignalComponent begin
+    L1C
+    L2C
+    L5I
+    L5Q
+    E1B
+    E1C
+    E5aI
+    E5aQ
+    E5bI
+    E5bQ
+    E6B
+    E6C
+end
+
+function get_constellations(::Navipedia)
+    return Constellations
+end
+
+function get_signal_bands(::Navipedia)
+    return SignalBands
+end
+
+function get_signal_types(::Navipedia)
+    return SignalComponents
+end
 
 end
